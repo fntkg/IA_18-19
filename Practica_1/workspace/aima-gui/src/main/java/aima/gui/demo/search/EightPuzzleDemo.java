4ada855e538253ea6006d6f79e756f9db4f0722f
fntkg <garces.g1999@gmail.com>
2020-04-08 00:25:33 +0200

    Añadidas practicas

---
+++ Practica_1/workspace/aima-gui/src/main/java/aima/gui/demo/search/EightPuzzleDemo.java
@@ -0,0 +1,94 @@
+package aima.gui.demo.search;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import aima.core.agent.Action;
+import aima.core.environment.eightpuzzle.EightPuzzleBoard;
+import aima.core.environment.eightpuzzle.EightPuzzleFunctionFactory;
+import aima.core.environment.eightpuzzle.EightPuzzleGoalTest;
+import aima.core.search.framework.Problem;
+import aima.core.search.framework.ResultFunction;
+import aima.core.search.framework.Search;
+import aima.core.search.framework.SearchAgent;
+import aima.core.search.uninformed.DepthLimitedSearch;
+import aima.core.search.uninformed.IterativeDeepeningSearch;
+
+/**
+ * @author Ravi Mohan
+ * 
+ */
+
+public class EightPuzzleDemo {
+	static EightPuzzleBoard boardWithThreeMoveSolution = new EightPuzzleBoard(
+			new int[] { 1, 2, 5, 3, 4, 0, 6, 7, 8 });;
+
+	static EightPuzzleBoard random1 = new EightPuzzleBoard(new int[] { 1, 4, 2,
+			7, 5, 8, 3, 0, 6 });
+
+	static EightPuzzleBoard extreme = new EightPuzzleBoard(new int[] { 0, 8, 7,
+			6, 5, 4, 3, 2, 1 });
+
+	public static void main(String[] args) {
+		eightPuzzleDLSDemo();
+		eightPuzzleIDLSDemo();
+	}
+
+	private static void eightPuzzleDLSDemo() {
+		System.out.println("\nEightPuzzleDemo recursive DLS (9) -->");
+		try {
+			Problem problem = new Problem(boardWithThreeMoveSolution, EightPuzzleFunctionFactory
+					.getActionsFunction(), EightPuzzleFunctionFactory
+					.getResultFunction(), new EightPuzzleGoalTest());
+			Search search = new DepthLimitedSearch(9);
+			SearchAgent agent = new SearchAgent(problem, search);
+			executeActions(agent.getActions(), problem);
+			printInstrumentation(agent.getInstrumentation());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void eightPuzzleIDLSDemo() {
+		System.out.println("\nEightPuzzleDemo Iterative DLS -->");
+		try {
+			Problem problem = new Problem(random1, EightPuzzleFunctionFactory
+					.getActionsFunction(), EightPuzzleFunctionFactory
+					.getResultFunction(), new EightPuzzleGoalTest());
+			Search search = new IterativeDeepeningSearch();
+			SearchAgent agent = new SearchAgent(problem, search);
+			executeActions(agent.getActions(), problem);
+			printInstrumentation(agent.getInstrumentation());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void printInstrumentation(Properties properties) {
+		Iterator<Object> keys = properties.keySet().iterator();
+		while (keys.hasNext()) {
+			String key = (String) keys.next();
+			String property = properties.getProperty(key);
+			System.out.println(key + " : " + property);
+		}
+
+	}
+	
+	public static void executeActions(List<Action> actions, Problem problem) {
+		Object initialState = problem.getInitialState();
+		ResultFunction resultFunction = problem.getResultFunction();
+		Object state = initialState;
+		System.out.println("INITIAL STATE");
+		System.out.println(state);
+		for (Action action : actions) {
+		 System.out.println(action.toString());
+		 state = resultFunction.result(state, action);
+		 System.out.println(state);
+		 System.out.println("- - -");
+		}
+	}
+
+}
